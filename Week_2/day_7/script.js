0dbe986c33d546d3e7c215a02d27cd149d88842d
JaimieGarcia <86097848+JaimieGarcia@users.noreply.github.com>
2021-07-03 15:29:35 -0700

    Debugged some issues, but not all

--- Week_2/day_7/script.js
+++ Week_2/day_7/script.js
@@ -14,7 +14,7 @@ let x = 5;
 
 // when you "set up" a variable, that's known as a declaration 
 let y;
-console.log("At this moment, y is undefined: " + y)
+console.log("At this moment, y is undefined: " + y);
 // when you set a value that's known as initialization 
 y = 3; 
 
@@ -23,18 +23,18 @@ let z = 6 + 7;
 console.log("the value z is " + z);
 
 // below is a tempalte literal that shows the value of x
-console.log(`the value of x is ${x}`)
+console.log(`the value of x is ${x}`);
 // below is concatanation that does the same thing
-console.log("the value of y is " + y)
-console.log(y)
+console.log("the value of y is " + y);
+console.log(y);
 
 const MY_CONST = 7;
 
-console.log(MY_CONST)
+console.log(MY_CONST);
 
 MY_CONST = 9;
 
-console.log(MY_CONST)
+console.log(MY_CONST);
 
 // the code below is not legal
 // you cannot reassign const variables
@@ -66,19 +66,19 @@ let myOtherBoolean = false;
 let myVariable = "abc";
 console.log(typeof myVariable);
 
-myVariable = 1
+myVariable = 1;
 console.log(typeof myVariable);
 
-let coercedVariable = 7 + 7 + 7
+let coercedVariable = 7 + 7 + 7;
 console.log(`the value of coercedVariable is ${coercedVariable}`);
 
 // JS will always resolve from top left to bottom right 
 // starts with the first two numbers and then thinks the next 7 is a string
-coercedVariable = 7 + 7 + "7"
+coercedVariable = 7 + 7 + "7";
 console.log(`the value of coercedVariable is ${coercedVariable}`);
 
 
-coercedVariable = "7" + 7 + 7
+coercedVariable = "7" + 7 + 7;
 console.log(`the value of coercedVariable is ${coercedVariable}`);
 
 let parsedInt = parseInt("10");
@@ -86,49 +86,50 @@ console.log(parsedInt);
 
 let foo = null;
 
-console.log(`This is the value of foo: ${foo}`)
+console.log(`This is the value of foo: ${foo}`);
 
 
 let conditional = true;
 
 if(conditional) {
-    console.log("The conditional was true.")
+    console.log("The conditional was true.");
 } else {
-    console.log("the conditional was false.")
+    console.log("the conditional was false.");
 }
 
 let numberConditional = 2;
 
-if (numberConditional === 5) {
-    console.log("The number is equal to 5")
+if (numberConditional === 5); {
+    console.log("The number is equal to 5");
 } else if (numberConditional > 3) {
-    console.log("The number is at least bigger than 3")
+    console.log("The number is at least bigger than 3");
 } else if (numberConditional < 10) {
-    console.log("The number is less than 10")
+    console.log("The number is less than 10");
 }
 
 let equalityCheckNumber = 10;
 
-if(equalityCheckNumber == "10") {
-    console.log("The values are equal")
+if(equalityCheckNumber == "10"); {
+    console.log("The values are equal");
 }
 
-if(equalityCheckNumber === "10") {
-    console.log("The values and types are equal")
+if(equalityCheckNumber === "10"); {
+    console.log("The values and types are equal");
 }
 
-if (true && false) {
+if (true && false); {
     console.log("This code will never be reached");
 }
 
-if (true && 5 > 15 {
-    console.log("This code will always be reached");
+if (true && 5 > 15); {
+    console.log("This code will always be reached"); 
+}
 
-if (true || false) {
+if (true || false); {
     console.log("as long as one of the conditions are met this message will be shown");
 }
 
-for(let i = 0; i < 10; i++) {
+for(let i = 0; i < 10; i++); {
 	console.log(i);
 	console.log('before break'); // will only print once
 	break; // exits the next loop
@@ -136,7 +137,7 @@ for(let i = 0; i < 10; i++) {
 }
 
 // i++ is the same as saying i = i + 1 
-for (let i = 0, i <= 9; i++) {
+for (let i = 0, i <= 9; i++); {
     console.log(i);
 }
 
