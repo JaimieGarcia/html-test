fd3687ef91299a626ca0c22a8cc4cc64b49a6820
JaimieGarcia <86097848+JaimieGarcia@users.noreply.github.com>
2021-07-03 15:29:53 -0700

    First draft of javascript homework

---
+++ Week_2/javascript-hw/script.js
@@ -0,0 +1,18 @@
+// Test copy
+console.log("Hello world");
+console.log("My name is Jaimie");
+
+
+// Challenge 1: Do you need more coffee?
+console.log("How many cups of coffee have you had today?")
+let y;
+
+if (y < 3) {
+    console.log("Yes, I'll take another cup of coffee.");
+} else if (y > 2) {
+        console.log("I think I'm okay for now.");
+}
+
+
+// Challenge 2: Does your car need an oil change?
+
