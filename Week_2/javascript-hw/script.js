ee8cf8854fc1a5e662024557b3c34100b7163fc3
JaimieGarcia <86097848+JaimieGarcia@users.noreply.github.com>
2021-07-03 15:54:36 -0700

    Finalized coffee challenge

--- Week_2/javascript-hw/script.js
+++ Week_2/javascript-hw/script.js
@@ -4,15 +4,11 @@ console.log("My name is Jaimie");
 
 
 // Challenge 1: Do you need more coffee?
-console.log("How many cups of coffee have you had today?")
-let y;
-
+let y = 1;
 if (y < 3) {
     console.log("Yes, I'll take another cup of coffee.");
 } else if (y > 2) {
         console.log("I think I'm okay for now.");
 }
 
-
 // Challenge 2: Does your car need an oil change?
-
