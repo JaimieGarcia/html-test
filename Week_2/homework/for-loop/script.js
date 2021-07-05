27e102790f6db90b36ebc7f543506e3f081d3415
JaimieGarcia <86097848+JaimieGarcia@users.noreply.github.com>
2021-07-05 14:14:50 -0700

    Removed comments

--- Week_2/homework/for-loop/script.js
+++ Week_2/homework/for-loop/script.js
@@ -1,8 +1,3 @@
-// Test copy
-console.log("Hello world");
-console.log("My name is Jaimie");
-
-
 // For loop homework assignment that will iterate from 0 to 99
 for (var x=0; x<=99; x++) {
     if (x === 0) {
