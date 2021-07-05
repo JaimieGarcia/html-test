22165f434c2a16c63160b05715068f155b2d40ba
JaimieGarcia <86097848+JaimieGarcia@users.noreply.github.com>
2021-07-05 13:47:24 -0700

    first commit For loop homework assignment

---
+++ Week_2/homework/for-loop/script.js
@@ -0,0 +1,17 @@
+// Test copy
+console.log("Hello world");
+console.log("My name is Jaimie");
+
+
+// For loop homework assignment that will iterate from 0 to 99
+for (var x=0; x<=99; x++) {
+    if (x === 0) {
+        console.log(x + " is even");
+    }
+    else if (x % 2 === 0) {
+        console.log(x + " is even");
+    }
+    else {
+        console.log(x + " is odd");
+    }
+}
