95ac803cdb68a6c9a559c00419bf9b1d229a2fda
JaimieGarcia <86097848+JaimieGarcia@users.noreply.github.com>
2021-07-06 07:56:37 -0700

    Fixed width

---
+++ Week_3/day_10/arrays.js
@@ -0,0 +1,24 @@
+let teachers = ["Shane", "Zack"];
+
+console.log(teachers.push ("Josh"));
+
+let arrayLength = teeachers.length;
+let oldTeacher = teachers.pop();
+
+teachers.unshift("New Guy");
+let newGuy = teachers.shift("Zack");
+
+teachers[5] = "Mister Fiveo";
+teachers[1] = "Lady Onsie";
+
+console.log("At index 20: " + teachers [20]);
+console.log("The array is now: " + arrayLength);
+console.log("The old teacher was: " + oldteacher);
+console.log(teachers);
+console.log("The new guy was: " + newGuy);
+
+teachers.unshift("One new person");
+teachers.unshift("Two new person");
+console.log(teachers);
+
+console.log("Index of Shane: " + teachers.indexOf("Shane"));
