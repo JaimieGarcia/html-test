ada42c9d31d6bfcd511537fc5cc6d62b702a7357
JaimieGarcia <86097848+JaimieGarcia@users.noreply.github.com>
2021-07-18 16:41:20 -0700

    Events

--- Week_3/javascript_for_beginners/main.js
+++ Week_3/javascript_for_beginners/main.js
@@ -1,1 +1,29 @@
-alert('File Linked');
+// alert('File Linked');
+
+// EVENTS: Custom Button
+// function doClick() {
+//     alert('You Clicked!');
+// }
+
+// EVENTS: A better way to do the above:
+// function changeText(id) {
+//     id.innerHTML = 'You Clicked!';
+// }
+
+// Changing the Heading after adding an id to html file
+// function changeText() {
+//     let heading = document.getElementById('heading');
+//     heading.innerHTML = 'You clicked!';
+// }
+
+// EVENTS: Show the date when you click or mouse over the button
+function showDate() {
+    let time = document.getElementById('time');
+    time.innerHTML = Date();
+}
+
+// EVENT: onMouseOut
+function clearDate() {
+    let time = document.getElementById('time');
+    time.innerHTML = '';
+}
