5d4bdc7beeef01cfbd340b50cc1da0c3e132705c
JaimieGarcia <86097848+JaimieGarcia@users.noreply.github.com>
2021-07-18 17:24:56 -0700

    Initial commit: Prepping for JS DOM crash course

---
+++ Week_4/javascript_dom_crash_course/DOM.js
@@ -0,0 +1,7 @@
+// the JavaScript DOM (Document Object Model) is and we will look at the different selectors like:
+// document.getElementById()
+// document.getElementsByClassName()
+// document.getElementsByTagName()
+// document.querySelector()
+// document.querySelectorAll()
+
