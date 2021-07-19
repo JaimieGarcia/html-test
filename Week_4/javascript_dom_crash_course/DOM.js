2e34cfc06d7ba1ca9bb0ad8a07be74e15f6c21c0
JaimieGarcia <86097848+JaimieGarcia@users.noreply.github.com>
2021-07-18 18:56:19 -0700

    Console log documents

--- Week_4/javascript_dom_crash_course/DOM.js
+++ Week_4/javascript_dom_crash_course/DOM.js
@@ -5,3 +5,23 @@
 // document.querySelector()
 // document.querySelectorAll()
 
+// EXAMINE THE DOCUMENT OBJECT 
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+
+// document.title = 123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+
+// document.all[10].textContent = 'Hello';
+// console.log(document.forms); 
+// console.log(document.links); 
+// console.log(document.images);
+
+// SELECTORS: GET ELEMENT BY ID 
+console.log(document.getElementByID('header-title'));
