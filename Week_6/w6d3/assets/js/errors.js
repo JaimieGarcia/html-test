628231103244a531abb68888bf80403685ff4471
JaimieGarcia <86097848+JaimieGarcia@users.noreply.github.com>
2021-07-28 08:41:41 -0700

    Initial commit

---
+++ Week_6/w6d3/assets/js/errors.js
@@ -0,0 +1,21 @@
+let fs = require("fs");
+
+// fs.readFile("Does/Not/Exist", handleFile);
+
+function handleFile(error, file) {
+    if (error) {
+        return console.error("Uh oh there was an error: " + error);
+    } else {
+    console.log("No error, here's your file!");
+    console.log(file);
+    }
+}
+
+try {
+    nonExistantFunction();
+} catch (error) {
+    console.error("Non existant function error: " + error);
+}
+
+nonExistantFunction
+console.log("Here is the next step!");
---
+++ Week_6/w6d3/assets/js/userInput.js
@@ -0,0 +1,5 @@
+let num = prompt("Enter a numerator:");
+let num = prompt("Enter a denomenator:");
+
+let result = Number.parseFloat(num) / Number.parseFloat(dom);
+console.log("Math result " + result);
