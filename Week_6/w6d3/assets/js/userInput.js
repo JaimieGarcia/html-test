bead5a57a24efd902c228dcb89f9f0cf9ecac143
JaimieGarcia <86097848+JaimieGarcia@users.noreply.github.com>
2021-07-28 08:59:54 -0700

    Tests

--- Week_6/w6d3/assets/js/userInput.js
+++ Week_6/w6d3/assets/js/userInput.js
@@ -1,5 +1,23 @@
-let num = prompt("Enter a numerator:");
-let num = prompt("Enter a denomenator:");
+"use strict"; // tells JS to be less forgiving 
+let num = "lol";
+let dom = "lmao";
+
+let num = prompt("Enter a numerator: ");
+let num = prompt("Enter a denominator: ");
+
+try {
+    num = Number.parseFloat(num);
+} catch (error) {
+    console.log("Numerator is not a real number! Setting to 1.")
+    num = 1;
+}
+
+try {
+    dom = Number.parseFloat(num);
+} catch (error) {
+    console.log("Denominator is not a real number! Setting to 1.")
+    dom = 1;
+}
 
 let result = Number.parseFloat(num) / Number.parseFloat(dom);
 console.log("Math result " + result);
