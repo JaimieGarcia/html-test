6b3ad0429d242cfc81514ecc9c188c9e6f4fadb1
JaimieGarcia <86097848+JaimieGarcia@users.noreply.github.com>
2021-07-29 07:55:53 -0700

    Solved

--- Week_6/w6d4/assets/js/exercise1.js
+++ Week_6/w6d4/assets/js/exercise1.js
@@ -1,17 +1,29 @@
 // Exercise 1: Rewrite the following code using async/await.
 
-function loadJson(url) {
-  return fetch(url)
-    .then(response => {
-      if (response.status == 200) {
-        return response.json();
-      } else {
+// function loadJson(url) {
+//   return fetch(url)
+//     .then(response => {
+//       if (response.status == 200) {
+//         return response.json();
+//       } else {
+//         throw new Error(response.status);
+//       }
+//     });
+// }
+
+// console.log(loadJson('http://httpstat.us/500').catch(alert)); // Error: 500
+
+let url ="https://javascript.info/article/promise-chaining/user.json";
+
+async function loadJson(url) {
+    let response = await fetch(url);
+    if (response.status == 200) {
+        let json = await response.json();
+        return json;
+    } else {
         throw new Error(response.status);
-      }
-    });
+    }
 }
 
-loadJson('http://httpstat.us/500')
-  .catch(alert); // Error: 500
-
-  
+let result = loadJson(url);
+loadJson(url);
