b20bce32f75a3d82eee98ac1f550d7785efd79c1
JaimieGarcia <86097848+JaimieGarcia@users.noreply.github.com>
2021-07-29 08:03:38 -0700

    Initial commit

---
+++ Week_6/w6d4/assets/js/exercise2.js
@@ -0,0 +1,15 @@
+// Call the async function in the regular function f(). 
+// How could we do this since we can't use await in f()?
+
+async function wait() {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+  
+    return 10;
+  }
+  
+  function f() {
+    // ...what should you write here?
+    // we need to call async wait() and wait to get 10
+    // remember, we can't use "await"
+  }
+
