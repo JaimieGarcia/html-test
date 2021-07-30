2d123523f91fc4b91303293df040ddf69288b868
JaimieGarcia <86097848+JaimieGarcia@users.noreply.github.com>
2021-07-29 22:48:43 -0700

    Working on the code

--- Week_6/w6d4/assets/js/homework2.js
+++ Week_6/w6d4/assets/js/homework2.js
@@ -11,3 +11,32 @@
 // so if you are getting errors and you think you shouldn't be, 
 // try calling fetching only ONCE per execution.)
 
+console.log("You are currently in " + cityName);
+async function cityName(fnParam) {
+    return fnParam('input value');
+}
+
+const API_KEY = 'YOUR_API_KEY_HERE';
+const API_URL = `https://geocode.xyz/seattle?json=1${API_KEY}&units=metric`;
+
+(function() {
+    'use strict';
+    
+    window.DI = window.DI || {};
+
+    let injectCoordinates = function injectCoordinates(coordinates) {
+        document.getElementById('latitude').innerHTML = coordinates.latitude;
+        document.getElementById('longitude').innerHTML = coordinates.longitude;
+    };
+
+    let getLocation = function getLocation(successCallback, failureCallback) {
+        navigator.geolocation.getCurrentPosition(
+            function resolveLocation(position) {
+                var coordinates = {
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                }
+    })
+
+    console.log("Your coordinates are: " + injectCoordinates);
+}});
