1ee13b89d9d2a41f9177bd8aae5d0f6eaac33981
JaimieGarcia <86097848+JaimieGarcia@users.noreply.github.com>
2021-08-02 07:51:05 -0700

    Class exercises

--- Week_7/w7d1/my-app/src/components/Board.js
+++ Week_7/w7d1/my-app/src/components/Board.js
@@ -1,9 +1,29 @@
 import React, {Component} from 'react';
-import '.../css/Board.css';
+import '../css/Board.css';
+import Note from './Note';
 
 class Board extends Component {
 
-    
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="div-board">
+                    <div className="row">
+                        <Note />
+                    </div>
+                </div>
+                <div>
+                    <button className="btn btn-success add-button">Add</button>
+                </div>
+            </div>
+        )
+    }
 
 
 }
+
+export default Board;
