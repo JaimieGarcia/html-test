32caddc4f5e061261f558e06a045f0ad1b48a49f
JaimieGarcia <86097848+JaimieGarcia@users.noreply.github.com>
2021-08-02 08:32:32 -0700

    Initial commit

---
+++ Week_7/w7d1/my-movies/src/components/Movie.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Movie = ({title, description, year, genre}) => {
+    return (
+        <div>
+            <h4></h4>
+            <h4></h4>
+            <h4></h4>
+            <h4></h4>
+        </div>
+    )
+}
+
+export default Movie;
